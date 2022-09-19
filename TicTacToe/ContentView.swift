201e0a23b9a65676328d8f00fb93cc19555e4255
Drakepal <playonplay23@gmail.com>
2022-09-19 14:15:22 -0500

    letters are added, spin effect works

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,18 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var moves: [String] = Array(repeating: "", count: 9)
+    @State var isPlaying = true
+    @State var gameOver = false
+    @State var msg = ""
+    
+    
     var body: some View {
         NavigationView {
             VStack {
                 LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
                     
                     ForEach(0..<9, id: \.self) { index in
-                        Color.white
-                            .frame(width: getWidth(),
-                            height: getWidth())
-                            .cornerRadius(15)
+                        ZStack {
+                            
+                            Color.blue
+                            
+                            Color.white
+                                .opacity(moves[index] == "" ? 1 : 0)
+                            
+                            Text(moves[index])
+                                .font(.system(size: 55))
+                                .fontWeight(.heavy)
+                                .foregroundColor(.white)
+                                .opacity(moves[index] != "" ? 1 : 0)
+                            
+                        }
+                        .frame(width: getWidth(),
+                        height: getWidth())
+                    .cornerRadius(15)
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] != "" ? 180 : 0), axis: (x:0.0, y: 1.0, z: 0.0),
+                        anchor: .center,
+                        anchorZ: 0.0,
+                        perspective: 1.0
+                        )
+                        
+                    .onTapGesture(perform : {
+                                    withAnimation(Animation.easeIn(duration: 0.5)) {
+                                        
+                                        if moves[index] == "" {
+                                        
+                                        moves[index] = isPlaying ? "X" : "O"
+                                        isPlaying.toggle()
+                                            
+                                }
+                            }
+                        })
                     }
-                    
                 }
                 .padding()
             }
